4c2f02c0c484b8c0906c105ce747877a6304e051
Jeffrey Johnson <ortelius@gmail.com>
2010-11-07 13:04:56 -0800

    Added databrowse, index view and static media serving for debug mode

--- urls.py
+++ urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls.defaults import *
+from django.contrib import databrowse
 
+from wmsb.models import WmsEndpoint, WmsLayer
+
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^django_wms_browser/', include('django_wms_browser.foo.urls')),
+databrowse.site.register(WmsEndpoint)
+databrowse.site.register(WmsLayer)
 
+urlpatterns = patterns('',
+    (r'^$', 'wmsb.views.index'),
+    (r'^wmsb/', include('django_wms_browser.wmsb.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^databrowse/(.*)', databrowse.site.root),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+    )
