b9fb2385459d6ec6b2c414178a6641f43869e3a2
Jeffrey Johnson <ortelius@gmail.com>
2010-11-08 00:47:32 -0800

    Added urlconfs for new views in wmsb.views for Server and Layer Listings

--- wmsb/urls.py
+++ wmsb/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls.defaults import *
 from wmsb.models import WmsEndpoint, WmsLayer
 
 urlpatterns = patterns('',
-	(r'^wms_layer/(?P<layer_id>\d+)/$', 'wmsb.views.layer'),
+	(r'^servers/$', 'wmsb.views.servers'),
+	(r'^server/(?P<server_id>\d+)/$', 'wmsb.views.server'),
+        (r'^layers/$', 'wmsb.views.layers'),
+	(r'^layer/(?P<layer_id>\d+)/$', 'wmsb.views.layer'),
+        (r'^register_url/$', 'wmsb.views.register_url'),
+        (r'^search/$', 'wmsb.views.search'),
+        (r'^about/$', 'wmsb.views.about'),
 )
