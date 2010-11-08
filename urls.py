817c2422b391b342520bcdfc3d836c443fbb50ec
Jeffrey Johnson <ortelius@gmail.com>
2010-11-08 00:45:55 -0800

    Changed index urlconf to point to wmsb.layers view Added login/logout urlconfs

--- urls.py
+++ urls.py
@@ -11,11 +11,13 @@ databrowse.site.register(WmsEndpoint)
 databrowse.site.register(WmsLayer)
 
 urlpatterns = patterns('',
-    (r'^$', 'wmsb.views.index'),
+    (r'^$', 'wmsb.views.layers'),
     (r'^wmsb/', include('django_wms_browser.wmsb.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^databrowse/(.*)', databrowse.site.root),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
 
 if settings.DEBUG:
